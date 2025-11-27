29b2d6d0b5d2f2add4c3fe558e4ceffc5bef5375
YooInHak <dlsgkr2856@gmail.com>
2025-11-27 18:09:42 +0900

    [FEAT] refresh토큰 제작

--- src/Routers/userRouter.js
+++ src/Routers/userRouter.js
@@ -1,11 +1,13 @@
 import { EXPRESS } from './../libs/constants.js';
 import { catchAsync } from './../libs/catchAsync.js';
-import { login, register } from '../controller/userController.js';
+import { login, register, refresh } from '../controller/userController.js';
+import auth from '../middlewares/auth.js';
 
 const userRouter = EXPRESS.Router();
 
 userRouter.post('/', catchAsync(register));
 userRouter.post('/login', catchAsync(login));
+userRouter.post('/token/refresh', auth.verifyRefreshToken, catchAsync(refresh));
 
 
 export default userRouter;
