0e9697afeb766371a0f2e50108520708e97737ce
YooInHak <dlsgkr2856@gmail.com>
2026-01-19 10:39:24 +0900

    [수정] comment관련 기능 분리

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,19 @@ import { RouterManager } from './Routers/routerManager';
 import { getCorsOrigin } from './libs/corsSetUp';
 import errorHandler from './libs/Handler/errorHandler';
 import { expressjwt } from 'express-jwt';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = EXPRESS();
-
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: getCorsOrigin(), // Express에서 쓰던 것과 똑같이 맞춰주세요
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+app.set('io', io);
 
 app.use(cors({
     origin: getCorsOrigin(),
@@ -30,6 +40,17 @@ app.use('/', RouterManager);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log(`Server is running`);
+// --- 소켓 연결 이벤트 (테스트용) ---
+io.on('connection', (socket) => {
+    console.log('새로운 소켓 연결:', socket.id);
+
+    // 로그인하면 해당 유저의 ID로 방을 만들어줌 (알림 기능을 위해 필수)
+    socket.on('join', (userId) => {
+        socket.join(userId);
+        console.log(`User ${userId} joined room`);
+    });
+});
+
+httpServer.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
